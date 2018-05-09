119cd798e93f427206c837195c517f9d7881ccb4
roger <roger.nadal.calafell@gmail.com>
2018-05-09 19:32:32 +0200

    cercle constructors

---
+++ Cercle.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package equip;
+
+/**
+ *
+ * @author Roger
+ */
+public class Cercle {
+private float x;
+private float y;
+
+public Cercle(){
+    this.x=1;
+    this.y=1;
+}
+
+public Cercle(Cercle c){
+    this.x=c.x;
+    this.y=c.y;
+}
+Cercle(float xx, float yy)
+{
+x=xx;
+y=yy;
+}
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Float.floatToIntBits(this.x);
+        hash = 97 * hash + Float.floatToIntBits(this.y);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cercle other = (Cercle) obj;
+        if (Float.floatToIntBits(this.x) != Float.floatToIntBits(other.x)) {
+            return false;
+        }
+        if (Float.floatToIntBits(this.y) != Float.floatToIntBits(other.y)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cercle{" + "x=" + x + ", y=" + y + '}';
+    }
+
+}
